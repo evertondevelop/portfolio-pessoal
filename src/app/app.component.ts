b5f80bd646cd6b9ca399bb2fb210b62fdc170b4c
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2025-05-29 20:33:22 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'portfolio-pessoal';
+  title = 'IT'S RUNNING MY FUCKING PORTFOLIO';
 }
